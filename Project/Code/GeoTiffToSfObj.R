29d3b930121c245a50eb9deb64196ae04216a620
Jake <jake.curry96@gmail.com>
2020-09-27 00:33:07 +0100

    Major changes to overlap calculation
    Hopefully running as wanted now

--- Project/Code/GeoTiffToSfObj.R
+++ Project/Code/GeoTiffToSfObj.R
@@ -310,7 +310,7 @@ ProcessTemm <- function(TemData) {
   return(NHM_Pangolins)
 }
 temminckii <- ProcessTemm("../Data/temmickiiOnly.csv")
-st_write(temminckii, "../Data/temmickiiProcessed.shp")
+# st_write(temminckii, "../Data/temmickiiProcessed.shp")
 
 NHM_Pangolins <- read.csv("../Data/temmickiiOnly.csv", header=T)
 NHM_Pangolins <- invisible(prepNHMData(NHM_Pangolins, 6))
@@ -324,11 +324,127 @@ myvars <- c('binomial', 'geometry')
 NHM_Pangolins <- NHM_Pangolins[myvars]
 NHM_Pangolins <- st_make_valid(NHM_Pangolins)
 class(NHM_Pangolins)
-IUCN <- readOGR("../Data/IUCN_Pholidota", "maps_pholidota")
-IUCN <- st_as_sf(IUCN)
-myvars <- c('binomial', 'geometry')
-IUCN <- IUCN[myvars]
+tem1 <- st_read("../Data/tem_div/temminckii_aoh_first")
+tem1$binomial = "Smutsia_temminckii"
+tem1 <- st_transform(tem1, 2163)
+calculateOverlaps(NHM_Pangolins, tem1)
+tem2 <- st_read("../Data/tem_div/temminckii_aoh_second")
+tem2$binomial = "Smutsia_temminckii"
+tem2 <- st_transform(tem2, 2163)
+calculateOverlaps(NHM_Pangolins, tem2)
+tem4 <- st_read("../Data/tem_div/temminckii_aoh_fourth")
+tem4$binomial = "Smutsia_temminckii"
+tem4 <- st_transform(tem4, 2163)
+calculateOverlaps(NHM_Pangolins, tem4)
 
+RunAOHAnalysis("../Data/NHMPangolinsCompatability.csv", "../Data/tem_Div/All")
+
+RunAOHAnalysis("../Data/NHMPangolinsCompatability.csv", "../Data/tem_Div/temminckii_aoh_fourth")
+
+temAoh <- st_read("../Data/tem_Div/temminckii_aoh_fourth")
+geom = list()
+for(i in 1:nrow(temAoh)) {
+  geom[[i]] = st_union(temAoh[temAoh[i, ], ])
+}
+geom = do.call(c, geom)
+### eg of how to dissolve a polygon, I think
+geom = list()
+for(i in 1:nrow(pol)) {
+  geom[[i]] = st_union(pol[pol[i, ], ])
+}
+geom = do.call(c, geom)
+
+
+#####POSSIBLE SOLUTION !!!!!! ######
+# treat AOH as have been treating NHM
+# Do cumulative percent overlaps
+# ie for each NHM specimen, 
+# go through each row of the AOH, adding to a list of percent overlaps
+# then once all overlaps calculated, add all values in list
+# that will be slow but will be the percentage overlap for that specimen
+#######################################################################
+for (row in NHMPangolins) {
+  percentList <- c()
+  for (entry in AOHDf) {
+    #calculate overlaps for row vs entry
+    overlap <- st_intersection(row, entry) %>% st_area() / st_area(row, entry) * 100
+    # at this point the output is of class "units" which don't play nice
+    overlap <- units::drop_units(overlap)
+    # allows for handling of cases of zero overlap
+    if (purrr::is_empty(overlap) == T) {
+      # as it otherwise returns a list of length zero,
+      # which cannot be appended to a df
+      overlap <- c(0)
+    }
+    #append to list
+    list.append(percentList, overlap)
+  }
+  #sum list 
+  sumOfPercentList = sum(percentList)
+  NHMPangolins$binomial <- sumOfPercentList
+}
+
+AOHOverlaps <- function(NHMPangolins, AOHDf) {
+  for (row in 1:nrow(NHMPangolins)) {
+    percentList <- c()
+    for (entry in 1:nrow(AOHDf)) {
+      NHMGeom <- NHMPangolins$geometry[row]
+      AOHGeom <- AOHDf$geometry[entry]
+      #calculate overlaps for row vs entry
+      overlap <- st_intersection(NHMGeom, AOHGeom) %>% st_area() / st_area(NHMGeom, AOHGeom) * 100
+      # at this point the output is of class "units" which don't play nice
+      overlap <- units::drop_units(overlap)
+      # allows for handling of cases of zero overlap
+      if (purrr::is_empty(overlap) == T) {
+        # as it otherwise returns a list of length zero,
+        # which cannot be appended to a df
+        overlap <- c(0)
+      }
+      #append to list
+      #list.append(percentList, overlap)
+      percentList <- rbind(overlap, percentList)
+    }
+    #sum list 
+    sumOfPercentList = sum(percentList)
+    NHMPangolins$percentOverlap <- sumOfPercentList #change to percent overlap lol 
+    # also make this do for each row but like stop and write out at the end of the row 
+    # and start on the next one like destrucivtly to untie memory
+    if (entry %% 5 == 0) {
+      st_write(NHMPangolins, paste("../Data/Overlaps/TempOverlaps", row, entry, ".csv", sep = "")) # writePangolinData to overlap file in a specific directory, then merge after
+    }
+  }
+  return(NHMPangolins)
+}
+
+temAoh <- st_read("../Data/tem_Div/temminckii_aoh_fourth")
+temAoh$binomial = "Smutsia_temminckii"
+temAoh <- st_transform(temAoh, 2163)
+NHM_Pangolins <- read.csv("../Data/temmickiiOnly.csv", header=T)
+NHM_Pangolins <- invisible(prepNHMData(NHM_Pangolins, 6))
+NHM_Pangolins <- NHM_Pangolins %>% filter(Decade != is.na(Decade))
+NHM_Pangolins <- NHM_Pangolins %>% select(-c(NOTES))
+NHM_Pangolins <- fixTypeNames(NHM_Pangolins)
+NHM_Pangolins$Extent..m. <- (NHM_Pangolins$Extent..m. /1000)
+NHM_Pangolins <- NHM_Pangolins %>% rename(Extent_km = Extent..m.)
+NHM_Pangolins <- addError(NHM_Pangolins)
+myvars <- c('binomial', 'geometry')
+NHM_Pangolins <- NHM_Pangolins[myvars]
+NHM_Pangolins <- st_make_valid(NHM_Pangolins)
+NHM_Pangolins<- st_transform(NHM_Pangolins, 2163)
+NHM_Pangolins$percentOverlap <- NA
+
+AOHOverlaps(NHM_Pangolins, temAoh)
+UnifyOverlapCSVs("../Data/Overlaps")
+
+#file.rename(list.files(pattern = "*"), paste("Smutsia_temmickii1_aoh"))
+
+# for (file in folder) {
+#   #rename file like this 
+#   ## Smutsia_temminckii[x]_aoh
+#   
+# }
+
+##### sandbox ######
 # 
 # NHM_Pangolins
 # st_rasterize(NHM_Pangolins, file = "../Data/temmickiiRaster.tif")
@@ -362,15 +478,32 @@ IUCN <- IUCN[myvars]
 # ggplot(p)
 
 
-#### trying again for temminckii
-temmFiles = list.files("../Data/tem_div", pattern = "*.shp", recursive = T, full.names = T)
-listOfFour = c("one", "two", "three", "four")
-for (file in temmFiles) {
-  listOfFour[file] = st_read(dsn = file)
-}
-NHMPangolinList <- ReadInAndProcessNHM("../Data/NHMPangolinsCompatability.csv")
-for (section in listOfFour) {
-  #pull out part of list
-  section
-  calculateOverlaps(NHMPangolinList, section)
-}
+# #### trying again for temminckii
+# temmFiles = list.files("../Data/tem_div", pattern = "*.shp", recursive = T, full.names = T)
+# listOfFour = c("one", "two", "three", "four")
+# for (file in temmFiles) {
+#   listOfFour[file] = st_read(dsn = file)
+# }
+# NHMPangolinList <- ReadInAndProcessNHM("../Data/NHMPangolinsCompatability.csv")
+# for (section in listOfFour) {
+#   #pull out part of list
+#   section
+#   calculateOverlaps(NHMPangolinList, section)
+# }
+
+# NHM_Pangolins <- read.csv("../Data/temmickiiOnly.csv", header=T)
+# NHM_Pangolins <- invisible(prepNHMData(NHM_Pangolins, 6))
+# NHM_Pangolins <- NHM_Pangolins %>% filter(Decade != is.na(Decade))
+# NHM_Pangolins <- NHM_Pangolins %>% select(-c(NOTES))
+# NHM_Pangolins <- fixTypeNames(NHM_Pangolins)
+# NHM_Pangolins$Extent..m. <- (NHM_Pangolins$Extent..m. /1000)
+# NHM_Pangolins <- NHM_Pangolins %>% rename(Extent_km = Extent..m.)
+# NHM_Pangolins <- addError(NHM_Pangolins)
+# myvars <- c('binomial', 'geometry')
+# NHM_Pangolins <- NHM_Pangolins[myvars]
+# NHM_Pangolins <- st_make_valid(NHM_Pangolins)
+# class(NHM_Pangolins)
+# IUCN <- readOGR("../Data/IUCN_Pholidota", "maps_pholidota")
+# IUCN <- st_as_sf(IUCN)
+# myvars <- c('binomial', 'geometry')
+# IUCN <- IUCN[myvars]
